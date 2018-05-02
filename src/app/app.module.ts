feece08432e204a70ceaafce94c3971186b6ff07
alvaro911 <algp232@hotmail.com>
2018-05-02 12:30:58 -0600

    demo from ngx-charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { APP_BASE_HREF, Location } from '@angular/common';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { NgxUIModule } from '@swimlane/ngx-ui';
 
 import { AppComponent } from './app.component';
+import { ComboChartComponent } from './combo-chart/combo-chart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComboChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NgxChartsModule,
+    NgxUIModule
   ],
-  providers: [],
+  providers: [{
+    provide: APP_BASE_HREF,
+    useFactory: getBaseLocation
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function getBaseLocation() {
+    const paths: string[] = location.pathname.split('/').splice(1, 1);
+    const basePath: string = (paths && paths[0]) || '';
+    return '/' + basePath;
+}
---
+++ src/app/chartTypes.ts
@@ -0,0 +1,83 @@
+const chartGroups = [
+  {
+    name: 'Bar Charts',
+    charts: [
+      {
+        name: 'Vertical Bar Chart',
+        selector: 'bar-vertical',
+        inputFormat: 'singleSeries',
+        options: [
+          'animations', 'colorScheme', 'schemeType', 'showXAxis', 'showYAxis', 'gradient', 'barPadding',
+          'showLegend', 'legendTitle', 'showXAxisLabel', 'xAxisLabel', 'showYAxisLabel', 'yAxisLabel',
+          'showGridLines', 'roundDomains', 'tooltipDisabled', 'roundEdges', 'yScaleMax'
+        ]
+      },
+      {
+        name: 'Horizontal Bar Chart',
+        selector: 'bar-horizontal',
+        inputFormat: 'singleSeries',
+        options: [
+          'animations', 'colorScheme', 'schemeType', 'showXAxis', 'showYAxis', 'gradient', 'barPadding',
+          'showLegend', 'legendTitle', 'showXAxisLabel', 'xAxisLabel', 'showYAxisLabel', 'yAxisLabel',
+          'showGridLines', 'roundDomains', 'tooltipDisabled', 'roundEdges', 'xScaleMax'
+        ],
+        defaults: {
+          yAxisLabel: 'Country',
+          xAxisLabel: 'GDP Per Capita',
+        }
+      },
+      {
+        name: 'Grouped Vertical Bar Chart',
+        selector: 'bar-vertical-2d',
+        inputFormat: 'multiSeries',
+        options: [
+          'animations', 'colorScheme', 'schemeType', 'showXAxis', 'showYAxis', 'gradient', 'barPadding', 'groupPadding',
+          'showLegend', 'legendTitle', 'showXAxisLabel', 'xAxisLabel', 'showYAxisLabel', 'yAxisLabel',
+          'showGridLines', 'roundDomains', 'tooltipDisabled', 'roundEdges', 'yScaleMax'
+        ]
+      },
+      {
+        name: 'Grouped Horizontal Bar Chart',
+        selector: 'bar-horizontal-2d',
+        inputFormat: 'multiSeries',
+        options: [
+          'animations', 'colorScheme', 'schemeType', 'showXAxis', 'showYAxis', 'gradient', 'barPadding', 'groupPadding',
+          'showLegend', 'legendTitle', 'showXAxisLabel', 'xAxisLabel', 'showYAxisLabel', 'yAxisLabel',
+          'showGridLines', 'roundDomains', 'tooltipDisabled', 'roundEdges', 'xScaleMax'
+        ],
+        defaults: {
+          yAxisLabel: 'Country',
+          xAxisLabel: 'GDP Per Capita',
+        }
+      },
+    ]
+  },
+  {
+    name: 'Pie Charts',
+    charts: [
+      {
+        name: 'Pie Chart',
+        selector: 'pie-chart',
+        inputFormat: 'singleSeries',
+        options: [
+          'animations', 'colorScheme', 'gradient', 'showLegend', 'legendTitle', 'doughnut', 'arcWidth',
+          'explodeSlices', 'showLabels', 'tooltipDisabled'
+        ]
+      },
+      {
+        name: 'Advanced Pie Chart',
+        selector: 'advanced-pie-chart',
+        inputFormat: 'singleSeries',
+        options: ['animations', 'colorScheme', 'gradient', 'tooltipDisabled']
+      },
+      {
+        name: 'Pie Grid',
+        selector: 'pie-grid',
+        inputFormat: 'singleSeries',
+        options: ['animations', 'colorScheme', 'tooltipDisabled']
+      }
+    ]
+  },
+];
+
+export default chartGroups;
